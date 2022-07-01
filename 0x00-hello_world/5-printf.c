708502a57ba4a35e0090ae40f6fac2202494804e
enenchedaddy <adogajeremiahbest@gmail.com>
2022-07-01 13:21:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-  */
-int main(void)
 
-	puts("\"Programming is like build\
-		         ing a multilingual puzzle");
-		        return (0);
+/**
+ * main -Enty point
+ *
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like build\
+			ing a mutillingual puzzle");
+	return (0);
 } 
