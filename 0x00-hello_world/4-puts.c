8b32d7437b4bb3747b9b2cf45677e20aafdb52a5
enenchedaddy <adogajeremiahbest@gmail.com>
2022-07-04 03:31:57 -0700

    jesus

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
+
+/**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("programming is like build\
-			ing a mutillingual puzzle");
+      	puts("\"Programming is like build\
+			ing a multilingual puzzle");
 	return (0);
 } 
