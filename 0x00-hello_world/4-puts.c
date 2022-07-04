4a8a84041736b02f51bf674f58c08739bdef226b
enenchedaddy <adogajeremiahbest@gmail.com>
2022-07-04 04:02:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-      	puts("\"Programming is like build\
+       	printf("\"Programming is like build\
 			ing a multilingual puzzle");
 	return (0);
 } 
