86faee0c321a774cd8b9dd8c4154d4443e2d60aa
enenchedaddy <adogajeremiahbest@gmail.com>
2022-07-04 19:28:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,7 +7,6 @@
  */
 int main(void)
 {
-       	printf("\"Programming is like build\
-			ing a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
